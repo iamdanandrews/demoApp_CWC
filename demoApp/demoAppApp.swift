a30968c4f863568c39c480744a7a768b20f2391e
“iamdanandrews” <hello@iamdanandrews.com>
2024-02-21 09:04:06 +0000

    Initial Commit

---
+++ demoApp/demoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoAppApp.swift
+//  demoApp
+//
+//  Created by Dan Andrews on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct demoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
